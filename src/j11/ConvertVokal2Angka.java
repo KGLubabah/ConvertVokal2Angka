659455a948f19f3d8ad58d504e255ccf69884afd
USER PC <USER PC@192.168.0.15>
2018-08-16 20:17:11 +0700

    Initial Commit

---
+++ src/j11/ConvertVokal2Angka.java
@@ -0,0 +1,15 @@
+package j11;
+
+public class ConvertVokal2Angka {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    String identitas = "Khairis Ghifariy Lubabah / XR3 / 15";
+    tampilJudul(identitas);
+    
+    
+}
