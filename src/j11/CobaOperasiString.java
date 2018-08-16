24b2cd251bde145f2c096e7b2bccecda0d586a72
USER PC <USER PC@192.168.0.15>
2018-08-16 19:26:33 +0700

    Initial Commit

---
+++ src/j11/CobaOperasiString.java
@@ -0,0 +1,10 @@
+package j11;
+
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = "Khairis Ghifariy Lubabah / XR3 / 15";
+        System.out.println("Identitas : " + identitas);
+    }
+}
+
